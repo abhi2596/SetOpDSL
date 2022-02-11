0ddd8b77c00d8fb0310a0f1e8d6e1dc3cea29313
abhi2596 <randy2596@gmail.com>
2022-02-11 07:23:58 -0600

    Set Operation DSL code with ScalaTest

--- src/main/scala/SetDSL.scala
+++ src/main/scala/SetDSL.scala
@@ -1,80 +1,75 @@
 object SetDSL:
-  // Variable binding are stored in binding
   private val binding : collection.mutable.Map[String,Any]= collection.mutable.Map("var"-> 1)
-  // SetName and its contents are stored in SetBinding
   private val SetBinding : collection.mutable.Map[String,Set[Any]]= collection.mutable.Map("someSetName" -> Set(3))
   private val MacroBinding : collection.mutable.Map[String,String]= collection.mutable.Map()
-  private val ScopeBinding : collection.mutable.Map[String,Any] = collection.mutable.Map()
+  private val ScopeBinding : collection.mutable.Map[String,collection.mutable.Map[String,Set[Any]]] = collection.mutable.Map()
   enum SetOp1:
     case Variable(Name:String)
-    case Value(Num:Any)
+    case Value(Val:Any)
     case SetName(Name: String)
-    case Check(op1:String,op2:SetOp1)
-    case Insert(op: SetOp1, arg: SetOp1 , arg1: SetOp1)
-    case Assign(x:SetOp1,y:SetOp1)
+    case Check(op1:SetOp1,op2:SetOp1)
+    case Insert(op: SetOp1, arg: SetOp1 , arg1:SetOp1) // used when setname is present in the set
+    case Assign(x:SetOp1,y:SetOp1,z:SetOp1) // used when setname is not present in the set binding
+    case Delete(x:SetOp1,y:SetOp1)
     case Union(x:SetOp1 , y: SetOp1)
     case Intersection(x:SetOp1,y:SetOp1)
     case SetDifference(x:SetOp1,y:SetOp1)
-    case CartesianProduct(x:String,y:String)
-    case SymmetricDifference(x:String,y:String)
+    case CartesianProduct(x:SetOp1,y:SetOp1)
+    case SymmetricDifference(x:SetOp1,y:SetOp1)
     case Macro(x:String,y:String)
-    case Scope(x:String,y:SetOp1)
+    case Scope(x:String,y:SetOp1,z:SetOp1,a:SetOp1)
 
     def eval: Any=
       this match{
-        // Returning the value of x passed as a set
         case Value(x)=> x
-        // Resolving the binding of variable
         case Variable(x)=>
           if (binding.contains(x))
             binding(x)
           else
             binding += (x->0)
             binding(x)
-        // just returning the string passed to method SetName
         case SetName(x) => x
-        // Checking whether a particular value is present in a set or not
-        case Check(op1,op2) => val set = SetBinding(op1.asInstanceOf[String])
-          println(set)
-          println(op2.eval)
+        case Check(op1,op2) => val set = SetBinding(op1.eval.asInstanceOf[String])
           if (set.contains(op2.eval.asInstanceOf[Any]))
             true
           else
             false
-        //      case Macro(x,y)=>   MacroBinding += (x->y)
-        //      MacroBinding(x)
-        // Creating a set with the given operands
+        case Assign(op1,op2,op3) =>
+          val setName = op1.eval.asInstanceOf[String]
+          val insert = Set(op2.eval,op3.eval)
+          if (SetBinding.contains(setName)) {
+              val set = SetBinding(setName) ++ insert
+              SetBinding += (setName -> set)
+            }
+          else {
+              SetBinding += (setName -> insert)
+            }
+          SetBinding(setName)
+
+
         case Insert(op1,op2,op3) =>
-          Set(op1.eval,op2.eval,op3.eval)
+          val setname = op1.eval.asInstanceOf[String]
+          val insert = Set(op2.eval,op3.eval) ++ SetBinding(setname)
+          SetBinding += (setname -> insert)
+          SetBinding(setname)
+
+        case Delete(op1,op2) =>
+          val name = op1.eval
+          if (SetBinding.contains(name.asInstanceOf[String])) {
+            SetBinding(name.asInstanceOf[String]) -= op2.eval.asInstanceOf[Any]
+            return SetBinding(name.asInstanceOf[String])
+          }
         case Union(op1,op2) =>
           SetBinding(op1.eval.asInstanceOf[String]).union(SetBinding(op2.eval.asInstanceOf[String]))
-
-        // Intersection of two sets
         case Intersection(op1,op2)=>
           SetBinding(op1.eval.asInstanceOf[String]).intersect(SetBinding(op2.eval.asInstanceOf[String]))
-        // Computes the SetDifference between two Sets whose names are passed to the method
         case SetDifference(op1,op2)=>
           SetBinding(op1.eval.asInstanceOf[String]).diff(SetBinding(op2.eval.asInstanceOf[String]))
-        // Assign creates a set if it is not present in the SetBinding
-        // and assigns the output of the second argument to the coressponding Set is available
-        // Else creates a new set with the name specified in op1 and assigns the output of
-        // second argument
-        case Assign(op1,op2) =>
-          val setName = op1.eval.asInstanceOf[String]
-          if (SetBinding.contains(setName))
-            val insert = op2.eval.asInstanceOf[Set[Any]].union(SetBinding(setName))
-            SetBinding += (setName->insert)
-            SetBinding(setName)
-          else
-            val insert = op2.eval.asInstanceOf[Set[Any]]
-            SetBinding += (setName -> insert)
-            SetBinding(setName)
-        // Computes the Symmetric Difference between two sets mentioned
-        case SymmetricDifference(x:String,y:String) =>
-          (SetBinding(x).union(SetBinding(y))).diff(SetBinding(y).intersect(SetBinding(x)))
-          // Computes the cartesian Product between two sets whose names are passed as argument     case CartesianProduct(x,y) =>
-          val Set1 = SetBinding(x)
-          val Set2 = SetBinding(y)
+        case SymmetricDifference(x,y) =>
+          (SetBinding(x.eval.asInstanceOf[String]).diff(SetBinding(y.eval.asInstanceOf[String]))).union(SetBinding(y.eval.asInstanceOf[String]).diff(SetBinding(x.eval.asInstanceOf[String])))
+        case CartesianProduct(x,y) =>
+          val Set1 = SetBinding(x.eval.asInstanceOf[String])
+          val Set2 = SetBinding(y.eval.asInstanceOf[String])
           val SetCp = collection.mutable.Set[Any]()
           for (i<-Set1) {
             for (j<-Set2){
@@ -82,15 +77,52 @@ object SetDSL:
             }
           }
           SetCp
-        case Scope(x,y) => ???
+
+        case Scope(x,y,z,a) =>
+          val setname = y.eval.asInstanceOf[String]
+          SetBinding += (setname-> Set(z.eval,a.eval))
+          if (ScopeBinding.contains(x)){
+            ScopeBinding(x)(setname) = ScopeBinding(x)(setname) ++ SetBinding(setname)
+            Delete(SetName(setname),z).eval
+            Delete(SetName(setname),y).eval
+            ScopeBinding
+          }
+          else {
+            ScopeBinding += (x -> collection.mutable.Map(setname -> SetBinding(setname)))
+            Delete(SetName(setname),z).eval
+            Delete(SetName(setname),y).eval
+            ScopeBinding
+          }
       }
   @main def runDSL : Unit =
     import SetOp1.*
-    var expression=Assign(SetName("someSetName"), Insert(Variable("var"), Value(2), Value("somestring"))).eval
-    expression = Check("someSetName",Value(1)).eval
-    println(SetBinding)
-    //    expression = Scope("scopename", Assign(SetName("someSetName"), Insert(Variable("var"), Value(1), Value("somestring")))).eval
-    println(expression)
-
+//    var expression = Insert(SetName("someSetName"),Value(1),Variable("x")).eval
+//    println(expression)
+//    expression = Assign(SetName("someSetName1"),Value(2),Variable("var")).eval
+//    expression = Assign(SetName("someSetName2"),Union(SetName("someSetName"),SetName("someSetName1"))).eval
+//    expression = Union(SetName("someSetName1"),SetName("someSetName")).eval
+//    println(expression)
+//    expression = Assign(SetName("someSetName1"),Variable("var"),Value("string")).eval
+//    println(expression)
+//    println(Check(SetName("someSetName1"), Value("string")).eval)
+//    var expression = Assign(SetName("someSetName"),Value(1),Value("somestring")).eval
+//    println(expression)
+//    println(SetBinding)
+//    expression = Insert(SetName("someSetName"),Variable("var"),Value(3)).eval
+//    expression = Union(SetName("someSetName1"),SetName("someSetName")).eval
+//    expression = Delete(SetName("someSetName"),Value(1)).eval
+//    var expression = Scope("name",SetName("someSetName"),Value(1),Value("somestring")).eval
+//    println(expression)
+//    expression = Scope("name",SetName("someSetName"),Value(2),Value("string")).eval
+//    expression = Scope("othername",SetName("someSetName"),Value(2),Value("string")).eval
+//    Scope("scopename", Scope("othername", Assign(Variable("someSetName"),Variable("var"), Value(1)), Value("somestring"))))
+//    println(expression)
+//    print(ScopeBinding)
+    var expression=Assign(SetName("SetName"),Value(2),Value(6)).eval
+    expression=Insert(SetName("SetName"),Value(7),Value(9)).eval
+    expression=Assign(SetName("SetName1"),Value(2),Value(4)).eval
+    expression = Insert(SetName("SetName1"),Value(6),Value(10)).eval
+    expression = SymmetricDifference(SetName("SetName"),SetName("SetName1")).eval
+    print(expression)
 
 
--- src/test/scala/SetDSLtest.scala
+++ src/test/scala/SetDSLtest.scala
@@ -2,11 +2,46 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import SetDSL.SetOp1.*
 
+import scala.collection.mutable
+
 class YourSetTheoryLanguageTest extends AnyFlatSpec with Matchers {
   behavior of "my first language for set theory operations"
 
   it should "create a set and insert objects into it" in {
-    Assign(SetName("someSetName"), Insert(Variable("var"), Value(2), Value("somestring"))).eval
-    Check("someSetName", Value(2)).eval shouldBe true
+    Insert(SetName("someSetName"),Value(1),Value("somestring")).eval
+    Assign(SetName("someSetName1"),Variable("var"),Value("string")).eval
+    Check(SetName("someSetName"), Value(1)).eval shouldBe true
+    Check(SetName("someSetName1"), Value("string")).eval shouldBe true
   }
+
+  it should "Delete elements in a Set" in {
+    Insert(SetName("someSetName"),Value(1),Value("somestring")).eval
+    Delete(SetName("someSetName"),Value("somestring")).eval
+    Check(SetName("someSetName"), Value("somestring")).eval shouldBe false
+  }
+
+  it should "test different set operations" in {
+    Assign(SetName("someSetName"),Value(1),Value(3)).eval
+    Insert(SetName("someSetName"),Value("string"),Value(5)).eval
+    Assign(SetName("someSetName1"),Value(2),Value("string")).eval
+    Insert(SetName("someSetName1"),Value(1),Value("somestring")).eval
+    val union = Union(SetName("someSetName"),SetName("someSetName1")).eval
+    union.asInstanceOf[Set[Any]] should contain allOf (1,2,3,"string","somestring",5)
+    val setDiff = SetDifference(SetName("someSetName"),SetName("someSetName1")).eval
+    setDiff.asInstanceOf[Set[Any]] should contain allOf (3,5)
+    val intersection = Intersection(SetName("someSetName"),SetName("someSetName1")).eval
+    intersection.asInstanceOf[Set[Any]] should contain allOf (1,"string")
+//    symmetricDifference.asInstanceOf[Set[Any]] should contain allOf ()
+    Assign(SetName("SetName"),Value(2),Value(6)).eval
+    Insert(SetName("SetName"),Value(7),Value(9)).eval
+    Assign(SetName("SetName1"),Value(2),Value(4)).eval
+    Insert(SetName("SetName1"),Value(6),Value(10)).eval
+    val symmetricDifference = SymmetricDifference(SetName("SetName"),SetName("SetName1")).eval
+    symmetricDifference.asInstanceOf[Set[Any]] should contain allOf (4,7,9,10)
+//    val cartesianProduct = CartesianProduct(SetName("setName"),SetName("setName1")).eval
+//    cartesianProduct should contain allOf mutable.HashSet(Set(1,3),Set(1,4),Set(2,3),Set(2,4))
+  }
+
+
+
 }
